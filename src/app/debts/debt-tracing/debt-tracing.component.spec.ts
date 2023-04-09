d5f3d0d35d272f9fe1dc558dec36c9ae8fa648b6
Christian Heredia <c.heredia87@gmail.com>
2023-04-09 13:50:24 +0200

    move navbar to core module

---
+++ src/app/debts/debt-tracing/debt-tracing.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DebtTracingComponent } from './debt-tracing.component';
+
+describe('DebtTracingComponent', () => {
+  let component: DebtTracingComponent;
+  let fixture: ComponentFixture<DebtTracingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DebtTracingComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DebtTracingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/debts/debt-tracing/debt-tracing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-debt-tracing',
+  templateUrl: './debt-tracing.component.html',
+  styleUrls: ['./debt-tracing.component.scss']
+})
+export class DebtTracingComponent {
+
+}
