79c0aafefc110c74370805548c16cd4f25633a54
Christian Heredia <cheredianaranjo.it@hbplat.com>
2023-12-28 19:33:08 +0100

    fix tslint

--- src/app/shared/pipes/exchange.pipe.ts
+++ src/app/shared/pipes/exchange.pipe.ts
@@ -2,14 +2,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { CurrencyService } from '../currency.service';
 
 @Pipe({
-  name: 'exchange'
+  name: 'exchange',
 })
 export class ExchangePipe implements PipeTransform {
-
   constructor(private currencyService: CurrencyService) {}
   transform(value: number = 0): number {
-    let exchangeCost = value * this.currencyService.getCurrencySettings().exchangeValue;
+    const exchangeCost =
+      value * this.currencyService.getCurrencySettings().exchangeValue;
     return exchangeCost;
   }
-
 }
