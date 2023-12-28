769e7b669435f8449699e71c54e6904dc9ec9921
Christian Heredia <cheredianaranjo.it@hbplat.com>
2023-12-29 00:26:48 +0100

    update sonar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component } from '@angular/core';
 import { WeatherService } from './forecast/shared/weather.service';
-import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterContentChecked {
   title = 'splity';
   weatherActive: boolean;
 
-  constructor(
-    private weatherService: WeatherService,
-    private router: Router
-    ){
+  constructor(private weatherService: WeatherService) {
     this.weatherActive = this.weatherService.getWeahterSettings().active;
   }
 
-ngOnInit(): void {}
-
-
-
-ngAfterContentChecked(): void {
-  //TODO change to behaviour emit
-  this.weatherActive = this.weatherService.getWeahterSettings().active;
-}
-
-
-
+  ngAfterContentChecked(): void {
+    //TODO change to behaviour emit
+    this.weatherActive = this.weatherService.getWeahterSettings().active;
+  }
 }
