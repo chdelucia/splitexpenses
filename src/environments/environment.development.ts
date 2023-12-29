8801bd8c797ad555ae547d8e18a105e219821a86
Christian Heredia <cheredianaranjo.it@hbplat.com>
2023-12-29 04:37:44 +0100

    dev env and en env

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  localStorageWeather: 'Wheater',
+  localStorageExpenses: 'Expenses',
+  localStorageUsers: 'Users',
+  localStorageSettings: 'Settings',
+  defaultCurrency: '€',
+  expensesTypes: ['Hobbies', 'Meals','Transport', 'Drinks', 'Culture',
+  'Accommodation','Souvenirs',  'Other'],
+  bgColors: ['#ff6384','#ff9f40','#ffcd56','#4bc0c0','#36a2eb','#9966ff',
+              '#c9cbcf','#ff4040'],
+  baseUrl: 'https://api.openweathermap.org/data/2.5/'
+};
