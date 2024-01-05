8fd1fe597376afb277b1738279ae7d2c0921ddc8
Christian Heredia <cheredianaranjo.it@hbplat.com>
2024-01-05 20:39:29 +0100

    redux debugger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { expensesReducer } from './state/expenses/expenses.reducer';
 import { CoreModule } from './core/core.module';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 
 @NgModule({
@@ -26,7 +27,14 @@ import { CoreModule } from './core/core.module';
     StoreModule.forRoot({users: userReducer, expenses: expensesReducer}),
     BrowserAnimationsModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    })
   ],
   providers: [
     LocalstorageService,
