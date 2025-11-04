8c9a1d5edfaa92d6b26328aca5c7f6ad695a3caf
Christian Heredia <cheredianaranjo.it@hbplat.com>
2025-11-04 17:46:42 +0100

    update angular 20

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,2 +1,17 @@
-//import 'jest-preset-angular/setup-jest';
-//import "@inrupt/jest-jsdom-polyfills"
+import 'jest-preset-angular/setup-jest';
+
+// Opcional: si usas global mocks o configuraciones extra
+// (por ejemplo para localStorage, ResizeObserver, etc.)
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
